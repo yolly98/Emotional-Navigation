d1e39a904d35fe3c08dbe60973b2f0f1b462526c
yolly98 <gianlucagemini98@gmail.com>
2023-04-17 13:15:52 +0200

    do stuff

--- Client/InputModule/test/gps_module_sim.py
+++ Client/InputModule/test/gps_module_sim.py
@@ -7,32 +7,75 @@ from Utility.point import Point
 from Utility.utility_functions import calculate_distance
 
 
-def sanitize_test_path(gps_data):
+def prepare_test_path(gps_data):
     gps_data['gps'].sort(key=lambda x: x['datetime'])
 
+    # remove duplicates
     new_gps_data = dict()
     new_gps_data['gps'] = []
     i = 0
-    progress = 0
     last_gps = None
+    duplicated_records = 0
     for gps in gps_data['gps']:
-        point = Point(gps['pos']['lat'], gps['pos']['lon'])
-        if not progress == math.floor((i * 100) / (len(gps_data['gps']))):
-            progress = math.floor((i * 100) / (len(gps_data['gps'])))
-            print(f"calculation {progress}%")
-
         if last_gps is not None:
-            last_point = Point(last_gps['pos']['lat'], last_gps['pos']['lon'])
             if gps['datetime'] == last_gps['datetime']:
-                print(f"duplicate record, datetime: {gps['datetime']}")
+                # print(f"duplicate record, datetime: {gps['datetime']}")
+                i += 1
+                duplicated_records += 1
                 continue
-            distance = calculate_distance(last_point, point)
-            if distance > 0.5:
-                print(f"too long distance: {distance}, last_point: {last_point}, point: {point}")
 
         new_gps_data['gps'].append(gps)
         last_gps = gps
         i += 1
+
+    print(f"removed {duplicated_records} duplicated records")
+    print("-------------------------------------")
+
+    # remove outliers
+    gps_data = new_gps_data
+    new_gps_data = dict()
+    new_gps_data['gps'] = []
+    i = 0
+    last_gps = None
+    outliers = 0
+    for gps in gps_data['gps']:
+        point = Point(gps['pos']['lat'], gps['pos']['lon'])
+
+        if last_gps is not None:
+            last_point = Point(last_gps['pos']['lat'], last_gps['pos']['lon'])
+            if gps['datetime'] == last_gps['datetime']:
+                print(f"duplicate record, Error!")
+                exit(1)
+
+            distance = calculate_distance(last_point, point)
+            if distance > 0.5:
+                print(f"too long distance: {distance}, last_point: {last_point}, point: {point}")
+
+                j = i + 1
+                next_pos = gps_data['gps'][j]
+                while next_pos == gps:
+                    if j < len(gps_data['gps']) - 1:
+                        j += 1
+                    else:
+                        break
+                    next_pos = gps_data['gps'][j]
+
+                if not next_pos == gps:
+                    next_point = Point(next_pos['pos']['lat'], next_pos['pos']['lon'])
+                    next_distance = calculate_distance(point, next_point)
+                    print(next_distance)
+                    if next_distance > 0.5:
+                        print(f"detected outlier: {gps}")
+                        outliers += 1
+                        i += 1
+                        continue
+
+        new_gps_data['gps'].append(gps)
+        last_gps = gps
+        i += 1
+
+    print(f"removed {outliers} outliers")
+    print("-------------------------------------")
 
     return new_gps_data
 
@@ -79,7 +122,7 @@ if __name__ == '__main__':
     with open('gps-test.json', 'r') as f:
         gps_data = json.load(f)
 
-    gps_data = sanitize_test_path(gps_data)
+    gps_data = prepare_test_path(gps_data)
     visualize_test_path(gps_data)
     # exit(0)
 
