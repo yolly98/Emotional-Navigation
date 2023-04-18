b93875bc11a27153f94fbfe658fbf638b93cf1c8
yolly98 <gianlucagemini98@gmail.com>
2023-04-18 16:49:18 +0200

    do stuff

--- Client/InputModule/test/graph_hopper_test.py
+++ Client/InputModule/test/graph_hopper_test.py
@@ -1,11 +1,13 @@
-# docker run --name graphhopper  -d -p 8989:8989 israelhikingmap/graphhopper --url https://download.geofabrik.de/europe/andorra-latest.osm.pbf --host 0.0.0.0
+# https://github.com/IsraelHikingMap/graphhopper-docker-image-push
+# docker run --name graphhopper -p 8989:8989 israelhikingmap/graphhopper --url https://download.geofabrik.de/europe/italy/centro-latest.osm.pbf --host 0.0.0.0
 import requests
-import polyline
 import matplotlib.pyplot as plt
 import math
-
-# Imposta la chiave API di GraphHopper
-api_key = '5041e9d4-34a5-43c5-9c32-b5025464b47f'
+import json
+from Utility.point import Point
+from Utility.gnode import GNode
+from Utility.way import Way
+from Utility.utility_functions import calculate_distance
 
 # Imposta la posizione di partenza e di arrivo
 start_point = '42.33399231261333, 12.269070539901804'
@@ -14,12 +16,17 @@ end_point = '42.332018371668575, 12.264471452495334'
 # Imposta la lista di ID degli archi da evitare
 avoid_edges = []
 
-def get_street_name(point):
-    # Costruisci l'URL per la richiesta di routing
-    url = f'https://graphhopper.com/api/1/route?point={point}&key={api_key}&details=street_name'
 
-    # Effettua la richiesta HTTP all'API di GraphHopper
-    response = requests.get(url)
+def get_nearest_point(point):
+
+    url = "http://localhost:8989/nearest"
+
+    params = {
+        "point": point,
+        "vehicle": "car"
+    }
+
+    response = requests.get(url, params=params)
 
     # Verifica che la richiesta sia andata a buon fine
     if response.status_code != 200:
@@ -29,29 +36,38 @@ def get_street_name(point):
     json_data = response.json()
     print(json_data)
 
+
 def get_path(start_point, end_point):
-    # Costruisci l'URL per la richiesta di routing
-    url = f'https://graphhopper.com/api/1/route?point={start_point}&point={end_point}&vehicle=car&locale=it&key={api_key}&avoid_edges={",".join(avoid_edges)}'
 
-    # Effettua la richiesta HTTP all'API di GraphHopper
-    response = requests.get(url)
+    url = "http://localhost:8989/route"
 
-    # Verifica che la richiesta sia andata a buon fine
+    params = {
+        "point": [start_point, end_point],
+        "profile": "car",
+        "instructions": "true",
+        "points_encoded": "false"
+    }
+
+    response = requests.get(url, params=params)
+
     if response.status_code != 200:
         print('Errore nella richiesta di routing:', response.text)
         exit()
 
-    # Estrai il percorso dalla risposta JSON
-    json_data = response.json()
+    json_data = json.loads(response.text)
     print(json_data)
     print("--------------------------")
     print(json_data['paths'])
     print("--------------------------")
     print(json_data['paths'][0]['instructions'])
     print("--------------------------")
-    points = polyline.decode(json_data['paths'][0]['points'])
-    print(points)
 
+    with open("path-example.json", "w") as f:
+        json.dump(json_data, f, indent=4)
+        f.close()
+
+    # print path
+    points = json_data['paths'][0]['points']['coordinates']
     fig, ax = plt.subplots()
     lats = []
     lons = []
@@ -77,6 +93,47 @@ def get_path(start_point, end_point):
     ax.axis('off')
     plt.show()
 
+    '''
+    # convert graphhopper path to my path
+    last_point = None
+    for point in points:
+        if last_point is None:
+            last_point = point
+            continue
+
+        way = dict()
+
+        start_node = GNode(
+            id=neo4j_start_node.get('id'),
+            type=neo4j_start_node.get('type'),
+            name=neo4j_start_node.get('name'),
+            lat=neo4j_start_node.get('lat'),
+            lon=neo4j_start_node.get('lon')
+        )
+        end_node = GNode(
+            id=neo4j_end_node.get('id'),
+            type=neo4j_end_node.get('type'),
+            name=neo4j_end_node.get('name'),
+            lat=neo4j_end_node.get('lat'),
+            lon=neo4j_end_node.get('lon')
+        )
+        way_properties = Way(
+            id=neo4j_way['way_id'],
+            name=neo4j_way['name'],
+            alt_name=neo4j_way['alt_name'],
+            ref=neo4j_way['ref'],
+            speed=neo4j_way['speed'],
+            length=neo4j_way['length'],
+            start_node=neo4j_way['start_node'],
+            end_node=neo4j_way['end_node']
+        )
+        way['way'] = way_properties
+        way['start_node'] = start_node
+        way['end_node'] = end_node
+        path.append(way)
+    '''
+
+
 if __name__=='__main__':
     get_path(start_point, end_point)
-    get_street_name(start_point)
+    get_nearest_point(start_point)
