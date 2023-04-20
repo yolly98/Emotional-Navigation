86bae7450aad2ee9fa1cef6f2ad68a191db9a89d
yolly98 <gianlucagemini98@gmail.com>
2023-04-20 12:42:38 +0200

    changed way_id with way name

--- Server/Persistence/mongo_history_manager.py
+++ Server/Persistence/mongo_history_manager.py
@@ -22,14 +22,14 @@ class MongoHistoryManager:
         self.conn.close()
         self.conn = None
 
-    def store_sample(self, username, way_id, emotion, timestamp):
+    def store_sample(self, username, way, emotion, timestamp):
         username = username.lower()
         if self.conn is None:
             return False
 
         sample = {
             'username': username,
-            'way_id': int(way_id),
+            'way': way,
             'emotion': emotion,
             'timestamp': timestamp
         }
@@ -41,7 +41,7 @@ class MongoHistoryManager:
             return False
         return True
 
-    def get_emotions(self, username, way_id):
+    def get_emotions(self, username, way):
         username.lower()
         if self.conn is None:
             return False
@@ -50,7 +50,7 @@ class MongoHistoryManager:
             {
                 '$and': [
                     {'username': username},
-                    {'way_id': way_id}
+                    {'way': way}
                 ]
             }
         )
@@ -69,6 +69,26 @@ class MongoHistoryManager:
         db = self.conn['smart_navigation'].history
         try:
             db.delete_many({'username': username})
+        except Exception as e:
+            print(f"Mongo execution error [{e}]")
+            return False
+        return True
+
+    def delete_history_by_user_way(self, username, way):
+
+        username = username.lower()
+        if self.conn is None:
+            return False
+        db = self.conn['smart_navigation'].history
+        try:
+            db.delete_many(
+                {
+                    '$and': [
+                        {'username': username},
+                        {'way': way}
+                    ]
+                }
+            )
         except Exception as e:
             print(f"Mongo execution error [{e}]")
             return False
@@ -105,14 +125,14 @@ class MongoHistoryManager:
 if __name__ == '__main__':
     history = MongoHistoryManager().get_instance()
     history.open_connection()
-    history.store_sample("user0", 0, 'happy', datetime.now())
-    history.store_sample("user0", 0, 'sad', datetime.now() + timedelta(minutes=1))
-    history.store_sample("user0", 1, 'disgust', datetime.now() + timedelta(minutes=2))
-    history.store_sample("user0", 1, 'angry', datetime.now() + timedelta(minutes=3))
-    history.store_sample("user0", 1, 'fear', datetime.now() + timedelta(minutes=4))
-    emotions = history.get_emotions("user0", 0)
-    print(f"way_id: {0} emotions: {emotions}")
-    emotions = history.get_emotions("user0", 1)
-    print(f"way_id: {1} emotions: {emotions}")
+    history.store_sample("user0", "Via Alfonso", 'happy', datetime.now())
+    history.store_sample("user0", "Via Alfonso", 'sad', datetime.now() + timedelta(minutes=1))
+    history.store_sample("user0", "Via Giacomo", 'disgust', datetime.now() + timedelta(minutes=2))
+    history.store_sample("user0", "Via Giacomo", 'angry', datetime.now() + timedelta(minutes=3))
+    history.store_sample("user0", "Via Giacomo", 'fear', datetime.now() + timedelta(minutes=4))
+    emotions = history.get_emotions("user0", "Via Alfonso")
+    print(f"way: Via Alfonso emotions: {emotions}")
+    emotions = history.get_emotions("user0", "Via Giacomo")
+    print(f"way: Via Giacomo emotions: {emotions}")
     history.delete_history_of_a_user("user0")
     history.close_connection()
