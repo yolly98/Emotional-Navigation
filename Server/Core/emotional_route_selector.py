022d5ed68295b09c66356caf8cb981882d9fe8ad
yolly98 <gianlucagemini98@gmail.com>
2023-04-20 13:47:03 +0200

    refactor emotional route selector

--- Server/Core/emotional_route_selector.py
+++ Server/Core/emotional_route_selector.py
@@ -1,6 +1,6 @@
 from Server.Persistence.mongo_history_manager import MongoHistoryManager
 from Server.Core.map_engine import MapEngine
-
+import json
 # emotion values for balance
 ANGRY = -5
 DISGUST = -1
@@ -10,50 +10,43 @@ SAD = -2
 SURPRISE = 5
 NEUTRAL = 1
 
-MAX_LENGTH_PENALTY = 0.1  # 10%
-
 
 class EmotionalRouteSelector:
 
     @staticmethod
     def get_path(username, source, destination):
 
-        path = MapEngine.calculate_path(source, destination)
-        if path is None:
+        paths = MapEngine.calculate_path(source, destination, [])
+        if paths is None:
             return None
         if username is None:
-            return path
-        evaluated_path = EmotionalRouteSelector.evaluate_path(path, username)
-        original_length = evaluated_path['length']
-        while True:
-            if evaluated_path['emotional_path'][0]['balance'] >= 0:
-                break
-            else:
-                new_path = MapEngine.calculate_path(source, destination, excluded_way=evaluated_path['emotional_path'][0]['way_id'])
-                if new_path is None:
-                    break
-                new_evaluated_path = EmotionalRouteSelector.evaluate_path(new_path, username)
-                if new_evaluated_path['overall_balance'] > evaluated_path['overall_balance'] \
-                        and evaluated_path['length'] + original_length * MAX_LENGTH_PENALTY > new_evaluated_path['length']:
-                    path = new_path
-                    evaluated_path = new_evaluated_path
-                else:
-                    print("second path is not better")
-                    break
-        return path
+            return paths[0]
+
+        evaluated_paths = []
+        for path in paths:
+            evaluated_path = EmotionalRouteSelector.evaluate_path(path, username)
+            evaluated_paths.append(evaluated_path)
+
+        # search the best emotional path
+        best_path = None
+        for evaluated_path in evaluated_paths:
+            if best_path is None or best_path['overall_balance'] < evaluated_path['overall_balance']:
+                best_path = evaluated_path
+
+        return best_path
 
     @staticmethod
     def evaluate_path(path, username):
 
         # get all emotions related to the path from user's history
         MongoHistoryManager.get_instance().open_connection()
-        emotional_path = []
-        length = 0
         overall_balance = 0
-        for way in path:
-            way_id = way['way'].get('id')
-            length += way['way'].get('length')
-            emotions = MongoHistoryManager.get_instance().get_emotions(username, way_id)
+        for way in path['ways']:
+
+            way_name = way['street_name']
+            if way_name == "":
+                continue
+            emotions = MongoHistoryManager.get_instance().get_emotions(username, way_name)
 
             emotion_balance = 0
             for emotion in emotions:
@@ -72,15 +65,11 @@ class EmotionalRouteSelector:
                 elif emotion == 'neutral':
                     emotion_balance += NEUTRAL
 
-            emotion_balance = emotion_balance * way['way'].get('length')
+            emotion_balance = emotion_balance * way['distance']
             overall_balance += emotion_balance
-            emotional_path.append({'way_id': way_id, 'balance': emotion_balance})
 
         MongoHistoryManager.get_instance().close_connection()
-        res = dict()
-        res['length'] = length
-        res['overall_balance'] = overall_balance
-        res['emotional_path'] = sorted(emotional_path, key=lambda k: k['balance'])
-        print(res)
+        evaluated_path = path.copy()
+        evaluated_path['overall_balance'] = overall_balance
 
-        return res
+        return evaluated_path
