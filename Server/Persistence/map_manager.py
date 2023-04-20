f0cc8c1e37126f13728f9185dae85cfc25db0168
yolly98 <gianlucagemini98@gmail.com>
2023-04-20 11:06:00 +0200

    added map-manager with graphhopper

---
+++ Server/Persistence/map_manager.py
@@ -0,0 +1,162 @@
+'''
+https://github.com/IsraelHikingMap/graphhopper-docker-image-push
+docker run --name graphhopper -d -p 8989:8989 israelhikingmap/graphhopper --url https://download.geofabrik.de/europe/italy/centro-latest.osm.pbf --host 0.0.0.0
+https://github.com/mediagis/nominatim-docker
+docker run -it \
+  -e PBF_URL=https://download.geofabrik.de/europe/italy/centro-latest.osm.pbf \
+  -p 8080:8080 \
+  --name nominatim \
+  mediagis/nominatim:4.2
+'''
+
+import requests
+import matplotlib.pyplot as plt
+import math
+import json
+import polyline
+
+
+class MapManager:
+
+    @staticmethod
+    def get_nearest_point(point):
+
+        url = "http://localhost:8989/nearest"
+
+        params = {
+            "point": point,
+            "vehicle": "car"
+        }
+
+        response = requests.get(url, params=params)
+
+        if response.status_code != 200:
+            print('Graphhopper error in nearest resource:', response.text)
+            return None
+
+        json_data = response.json()
+        return json_data['coordinates']
+
+    @staticmethod
+    def get_point_by_location(location_name):
+
+        params = {
+            "q": location_name,
+            "format": "jsonv2",
+            "limit": 1
+        }
+
+        response = requests.get("http://localhost:8080/search", params=params)
+        if response.status_code != 200:
+            print('Nominatim error:', response.text)
+            return None
+
+        location = response.json()[0]
+        return [location["lon"], location["lat"]]
+
+    @staticmethod
+    def plot_path(path):
+
+        points = polyline.decode(path['points'])
+        print(points)
+        fig, ax = plt.subplots()
+        lats = []
+        lons = []
+        i = 0
+        progress = 0
+        last_point = None
+        for point in points:
+            if not progress == math.floor((i * 100) / (len(points))):
+                progress = math.floor((i * 100) / (len(points)))
+                print(f"calculation {progress}%", end="\r")
+
+            if last_point is not None:
+                ax.plot([point[0], last_point[0]], [point[1], last_point[1]], c='violet', alpha=1, linewidth=1)
+
+            lats.append(float(point[1]))
+            lons.append(float(point[0]))
+            last_point = point
+            i += 1
+
+        ax.scatter(lons, lats, c='white', alpha=1, s=3)
+        fig.set_facecolor('black')
+        ax.set_title("Path")
+        ax.axis('off')
+        plt.show()
+
+    @staticmethod
+    def get_path(start_point, end_point, avid_ways):
+        url = "http://localhost:8989/route"
+
+        params = {
+            "point": [start_point, end_point],
+            "profile": "car",
+            "instructions": "true",
+            "points_encoded": "true",
+            "locale": "it-IT",
+            "details": "max_speed",
+            "algorithm": "alternative_route",
+            "alternative_route.max_paths": 3,
+            "alternative_route.max_weight_factor": 1.5
+
+        }
+
+        response = requests.get(url, params=params)
+
+        if response.status_code != 200:
+            print('Errore nella richiesta di routing:', response.text)
+            return None
+
+        json_data = json.loads(response.text)
+        ordered_paths = sorted(json_data['paths'], key=lambda x: x['distance'])
+
+        # refactor path
+        paths = []
+        for i in range(0, len(ordered_paths)):
+            path = dict()
+            all_distance = ordered_paths[i]['distance']
+            all_time = ordered_paths[i]['time']
+            instructions = ordered_paths[i]['instructions']
+            points = ordered_paths[i]['points']
+            max_speed = ordered_paths[i]['details']['max_speed']
+            path['distance'] = all_distance
+            path['time'] = all_time
+            path['points'] = points
+            path['ways'] = instructions
+            path['max_speed'] = max_speed
+            avoided = False
+            for segment in instructions:
+                if segment['street_name'] in avid_ways:
+                    avoided = True
+                    break
+            if avoided:
+                continue
+            paths.append(path)
+
+
+        return paths
+
+
+if __name__ == '__main__':
+    start_point = '42.415832,12.106822'
+    end_point = '42.41942,12.10429'
+
+    paths = MapManager.get_path(start_point, end_point, [])
+    for i in range(0, len(paths)):
+        print("---------------------------------------------------")
+        print(json.dumps(paths[i], indent=4))
+        MapManager.plot_path(paths[i])
+
+    paths = MapManager.get_path(start_point, end_point, ["Via Cavour",])
+    for i in range(0, len(paths)):
+        print("---------------------------------------------------")
+        print(json.dumps(paths[i], indent=4))
+        MapManager.plot_path(paths[i])
+
+    '''
+    point = MapManager.get_point_by_location('Via VIII Ottobre')
+    point = MapManager.get_nearest_point(point)
+    path = MapManager.get_path(start_point, point)
+    print(json.dumps(path, indent=4))
+    MapManager.plot_path(path)
+    '''
